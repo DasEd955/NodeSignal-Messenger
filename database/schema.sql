1f2c52c5e4c2367432e9969fa1c4610f7dedcdf8
DasEd955 <info@tunerdesign.org>
2026-03-27 01:54:06 -0400

    Documented schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,16 @@
+/* ===================================================================================
+schema.sql -- Defines the SQLite Schema for Persisting Users & Messages
+=================================================================================== */
+
+-- Creates the Users Table
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     created_at INTEGER NOT NULL
 );
 
+-- Creates the Messages Table
+    -- sender_id links each message to the user who sent it
 CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sender_id INTEGER NOT NULL REFERENCES users(id),
