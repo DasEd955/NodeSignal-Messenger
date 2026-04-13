f70bfd4a903727a22180e7f87afb0b500e953af5
DasEd955 <info@tunerdesign.org>
2026-04-13 11:16:05 -0400

    Update client.h
    - client.h fully documented

--- client/client.h
+++ client/client.h
@@ -1,6 +1,19 @@
+/* ===================================================================================
+client.h -- Declares the public interface for the client component
+=================================================================================== */
+
 #ifndef NS_CLIENT_H
 #define NS_CLIENT_H
 
+/* int ns_client_run -- Runs the NodeSignal client application
+
+    -- int argc: The number of CLI arguments passed to the program
+    -- char **argv: The array of CLI argument strings
+    
+    -- Used as the main public entry point for starting the client application 
+    -- Initializes the networking layer, creates the GTK application, and runs the client event loop
+    -- Returns the client application's exit status 
+    */
 int ns_client_run(int argc, char **argv);
 
 #endif
